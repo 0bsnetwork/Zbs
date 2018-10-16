a507585f245e8891db5bbddb102a4f405a2d9e37
James <james@theapplicationist.com>
2018-10-16 12:28:42 +0200

    Remove smart assets until next version

--- src/main/scala/com/zbsnetwork/settings/BlockchainSettings.scala
+++ src/main/scala/com/zbsnetwork/settings/BlockchainSettings.scala
@@ -79,7 +79,7 @@ object FunctionalitySettings {
     allowMultipleLeaseCancelTransactionUntilTimestamp = 0,
     resetEffectiveBalancesAtHeight = 1,
     blockVersion3AfterHeight = 0,
-    preActivatedFeatures = Map[Short, Int]((1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0)),
+    preActivatedFeatures = Map[Short, Int]((1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0)),
     doubleFeaturesPeriodsAfterHeight = 0
   )
 
